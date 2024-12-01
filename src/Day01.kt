c6abec289e6a398eef83c1f4e38f504f4cb2acc6
justTill <till.pilarcti@web.de>
2024-12-01 20:48:47 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(input)
+    var (leftList, rightList) = input
+        .map { line ->
+            val (left, right) = line.split("   ").map { it.toInt() }
+            left to right
+        }
+        .unzip()
+    leftList = leftList.sorted()
+    rightList= rightList.sorted()
+    val differences = leftList.zip(rightList) { left, right -> Math.abs(left - right) }
+    println(differences.sum())
+
+
+
+    //part two
+    val rightListNumberCount = rightList.groupingBy { it }.eachCount();
+    val score = leftList.sumOf { left -> left * (rightListNumberCount[left] ?:0) }
+    println(score)
 }
