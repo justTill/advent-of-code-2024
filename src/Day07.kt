0491a53491019163c1e6659df5e9f388e04dd297
justTill <till.pilarcti@web.de>
2024-12-07 14:29:21 +0100

    wip day07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,16 +1,3 @@
-fun canBeCalculatedOlf(result: Long, values: List<Int>): Boolean {
-    val resultAddFirst:Long = values.foldIndexed(0) { index, acc, value ->
-        if (index % 2 == 0) acc + value else acc * value
-    }
-
-    var resultMultFirst:Long = 1
-    // Multiply first, then add
-    resultMultFirst = values.foldIndexed(1) { index, acc, value ->
-        if (index % 2 == 0) acc * value else acc + value
-    }
-
-    return result == resultAddFirst || result == resultMultFirst
-}
 fun canBeCalculated(result: Long, values: List<Long>): Boolean {
     val operatorCombinations = generateOperatorCombinations((values.size - 1).toLong())
 
@@ -37,6 +24,7 @@ fun generateOperatorCombinations(numOperators: Long): List<List<Char>> {
 
 fun evaluateExpression(values: List<Long>, operators: List<Char>): Long {
     var result = values[0]
+    println(values)
     for (i in 1 until values.size) {
         val operator = operators[i - 1]
         val value = values[i]
